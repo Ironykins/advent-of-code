613e1ae676d8f5aabd0a756e0cb478df21c32f7d
Ironykins <konrad.aust@gmail.com>
2016-06-02 18:09:29 -0700

    Added solutions for day 24

---
+++ day24.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 konrad <konrad@serenity>
+#
+# Sleigh Balancing
+from itertools import chain, combinations
+from functools import reduce
+from operator import mul
+
+weights = []
+groupWeight = 0 # The number of weights.
+
+with open('day24.txt') as inFile:
+    for line in inFile:
+        weights.append(int(line.strip()))
+
+# Change this to division by 4 for part b
+groupWeight = sum(weights) // 3
+
+# Find all divisions with equal weight.
+min_entanglement = 999999999999999
+for packageNum in range(2,len(weights)):
+    for arrangement in combinations(weights, packageNum):
+        if sum(arrangement) == groupWeight:
+            min_entanglement = min(min_entanglement, reduce(mul,arrangement))
+
+    if min_entanglement < 999999999999999:
+        break
+
+print min_entanglement
